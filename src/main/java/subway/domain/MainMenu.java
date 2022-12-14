ef12036140ca51f1cd66b1e69007a00627ee5757
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 03:12:48 +0900

    feat: 메인화면 Enum Class 생성

--- src/main/java/subway/domain/MainMenu.java
+++ src/main/java/subway/domain/MainMenu.java
@@ -1,2 +1,21 @@
-package subway.domain;public enum MainMenu {
+package subway.domain;
+
+public enum MainMenu {
+    STATION("1","역 관리"),LINE("2","노선 관리"),SECTION("3","구간 관리"),MAP("4","지하철 노선도 출력"),END("Q","종료");
+
+    final private String command;
+    final private String message;
+
+    MainMenu(String command, String message) {
+        this.command = command;
+        this.message = message;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
