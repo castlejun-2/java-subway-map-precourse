6ba55fc0349aedb3ef7a135fb7a6f7d0aa0be2a4
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 03:13:15 +0900

    feat: 메인화면 명령어 입력 기능 구현

--- src/main/java/subway/domain/SubwayMap.java
+++ src/main/java/subway/domain/SubwayMap.java
@@ -13,7 +13,10 @@ public class SubwayMap {
     }
 
     public void play() {
-
+        while (true) {
+            OutputView.printArray(Message.MAIN_MESSAGE);
+            String inputData = InputView.getMainCmd(scanner);
+        }
     }
 
     public void initData() {
@@ -21,6 +24,14 @@ public class SubwayMap {
         initLine();
     }
 
-    public void initStation() {}
-    public void initLine() {}
+    public void initStation() {
+        String stations[] = {"교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역"};
+        for (String station : stations)
+            stationRepository.addStation(new Station(station));
+    }
+    public void initLine() {
+        String lines[] = {"2호선", "3호선", "신분당선"};
+        for (String line : lines)
+            lineRepository.addLine(new Line(line));
+    }
 }
