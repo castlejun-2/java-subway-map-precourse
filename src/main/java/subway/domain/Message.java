2715c439a5b932517267009584d9875320779981
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 03:12:00 +0900

    feat: 명령어 예외 발생 문자열 생성

--- src/main/java/subway/domain/Message.java
+++ src/main/java/subway/domain/Message.java
@@ -1,5 +1,6 @@
 package subway.domain;
 
 public class Message {
-    public static final String[] mainMsg = {"## 메인 화면", "1. 역 관리", "2. 노선 관리", "3. 구간 관리", "4. 지하철 노선도 출력", "Q. 종료"};
+    public static final String[] MAIN_MESSAGE = {"## 메인 화면", "1. 역 관리", "2. 노선 관리", "3. 구간 관리", "4. 지하철 노선도 출력", "Q. 종료"};
+    public static final String COMMAND_ERROR = "주어진 명령어만 입력 가능합니다.";
 }
