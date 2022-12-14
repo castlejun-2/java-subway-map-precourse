f3f91b56a5306bbceace01d2809d3435cfe9169d
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 02:43:02 +0900

    feat: 문자열 배열 출력문 생성

---
+++ src/main/java/subway/domain/OutputView.java
@@ -0,0 +1,8 @@
+package subway.domain;
+
+public class OutputView {
+    public static void printArray(String[] msg) {
+        for (int i = 0; i < msg.length; i++ )
+            System.out.println(msg[i]);
+    }
+}
