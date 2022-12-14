f0bb8262d467e0135ae6dc2cf2e7bc4257facb9d
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 01:56:17 +0900

    feat: SubwayMap 프로그램을 관리하는 SubwayMap Class 선언

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,13 @@
 package subway;
 
+import subway.domain.SubwayMap;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+
+        new SubwayMap(scanner).play();
     }
 }
---
+++ src/main/java/subway/domain/SubwayMap.java
@@ -0,0 +1,17 @@
+package subway.domain;
+
+import java.util.Scanner;
+
+public class SubwayMap {
+    private Scanner scanner;
+    private LineRepository lineRepository;
+    private StationRepository stationRepository;
+
+    public SubwayMap(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    public void play() {
+
+    }
+}
