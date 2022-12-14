7360da087b5737c46178d2c4373430517d00dcbe
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 03:19:04 +0900

    refactor: Enum Class를 활용해 출력문 구현

--- src/main/java/subway/domain/OutputView.java
+++ src/main/java/subway/domain/OutputView.java
@@ -1,9 +1,11 @@
 package subway.domain;
 
 public class OutputView {
-    public static void printArray(String[] msg) {
-        for (int i = 0; i < msg.length; i++ )
-            System.out.println(msg[i]);
+    public static void printMain() {
+        System.out.println("## 메인 화면");
+        for (MainMenu mainMenu : MainMenu.values()) {
+            System.out.println(mainMenu.getCommand()+". "+mainMenu.getMessage());
+        }
         System.out.println("");
     }
 
