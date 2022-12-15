59c07c72622897c222a8500765658b483750ffa5
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 18:30:11 +0900

    feat: 노선에 위치한 각 역의 초기값 삽입을 위한 함수 생성

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,15 @@
 package subway.domain;
 
+import subway.view.InputView;
+import subway.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Line {
     private String name;
+    private List<Station> stations = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -11,5 +19,11 @@ public class Line {
         return name;
     }
 
-    // 추가 기능 구현
+    public void addStation(Station[] station) {
+        stations.addAll(List.of(station));
+    }
+
+    public void addStationByIndex(Station station, int index) {
+        stations.add(index,station);
+    }
 }
