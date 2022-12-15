6b7c71848495375358727f8a5448539087ddb0e7
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 20:09:50 +0900

    feat: 세부 메뉴 Enum Class 생성

---
+++ src/main/java/subway/utils/ServiceMenuDetail.java
@@ -0,0 +1,31 @@
+package subway.utils;
+
+public enum ServiceMenuDetail {
+    STATION("1", Message.STATION_DETAIL_MESSAGE),
+    LINE("2", Message.LINE_DETAIL_MESSAGE),
+    SECTION("3", Message.SECTION_DETAIL_MESSAGE);
+
+    final private String command;
+    final private String[] message;
+
+    ServiceMenuDetail(String command, String[] message) {
+        this.command = command;
+        this.message = message;
+    }
+
+    public String[] findMessageByCommand(String cmd) {
+        for (ServiceMenuDetail serviceMenuDetail : ServiceMenuDetail.values()) {
+            if (serviceMenuDetail.getCommand().equals(cmd))
+                return serviceMenuDetail.getMessage();
+        }
+        return new String[]{};
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String[] getMessage() {
+        return message;
+    }
+}
