6ccfdfa044471838dbe7fd713758b53f5e146dca
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 19:02:46 +0900

    feat: 원하는 기능 선택 문자열 상수 선언

--- src/main/java/subway/utils/Message.java
+++ src/main/java/subway/utils/Message.java
@@ -3,6 +3,7 @@ package subway.utils;
 public class Message {
     public static final String COMMAND_ERROR = "주어진 명령어만 입력 가능합니다.";
 
+    public static final String SELECT_FUNCTION_MESSAGE = "원하는 기능을 선택하세요.";
     public static final String LINE_INPUT_MESSAGE = "노선을 입력하세요.";
 
     public static final String LINE_IS_NOT_EXIST = "존재하지 않는 노선입니다.";
