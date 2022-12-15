4676276bdab0d2eedfe276c7016f457948a87426
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 18:35:59 +0900

    feat: 노선 입력 메시지 삽입

--- src/main/java/subway/utils/Message.java
+++ src/main/java/subway/utils/Message.java
@@ -2,4 +2,8 @@ package subway.utils;
 
 public class Message {
     public static final String COMMAND_ERROR = "주어진 명령어만 입력 가능합니다.";
+
+    public static final String LINE_INPUT_MESSAGE = "노선을 입력하세요.";
+
+    public static final String LINE_IS_NOT_EXIST = "존재하지 않는 노선입니다.";
 }
