ef7d7f0a573e686b0e85685d95c96be9a2c00a49
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-15 18:35:41 +0900

    style: 공백 제거

--- src/main/java/subway/utils/Validator.java
+++ src/main/java/subway/utils/Validator.java
@@ -1,7 +1,11 @@
 package subway.utils;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.utils.MainMenu;
 import subway.utils.Message;
+
+import java.util.List;
 
 public class Validator {
     public static void checkMainCommand(String inputData) {
