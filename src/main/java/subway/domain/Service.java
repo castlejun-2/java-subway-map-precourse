02124589d063a180d4d82cc4cf4573270c6b2983
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 00:38:30 +0900

    feat: 메인 화면 이후의 하위 메뉴 Controller 구현

--- src/main/java/subway/domain/Service.java
+++ src/main/java/subway/domain/Service.java
@@ -1,11 +1,40 @@
 package subway.domain;
 
+import subway.domain.Controller.StationController;
 import subway.utils.ServiceMenu;
+import subway.view.InputView;
 import subway.view.OutputView;
 
-public class Service {
+import java.util.Scanner;
 
-    public Service(String input) {
-        OutputView.printServiceMenu(input);
+public class Service {
+    public static LineRepository lineRepository;
+    public static StationRepository stationRepository;
+
+    public Service(LineRepository lineRepository, StationRepository stationRepository){
+        this.lineRepository = lineRepository;
+        this.stationRepository = stationRepository;
+    }
+
+    public LineRepository lineRepository() { return lineRepository; }
+    public StationRepository stationRepository() { return stationRepository; }
+
+    public static void play(Scanner scanner, String input) {
+        String inputData = InputView.getDetailCmd(input, scanner);
+        if (inputData.equals("B"))
+            return;
+        selectService(scanner, input, inputData);
+    }
+
+    public static void selectService(Scanner scanner, String input, String inputDetail) {
+        if (input.equals("1")) {
+            StationController stationController = new StationController(inputDetail, stationRepository,lineRepository);
+            stationController.play(scanner);
+            stationRepository = stationController.getNewStationRepository();
+        }
+        //else if (input.equals("2"))
+        //    new LineController(inputDetail,lineRepository).play();
+        //else if (input.equals("3"))
+        //    new SectionController(inputDetail,stationRepository,lineRepository).play();
     }
 }
