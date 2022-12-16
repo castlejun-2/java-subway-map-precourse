1c7022fdde26f23c7071823a906f9dd26c36ac9c
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 00:34:59 +0900

    feat: 역을 관리하는 클래스 구현

---
+++ src/main/java/subway/domain/Controller/StationController.java
@@ -0,0 +1,47 @@
+package subway.domain.Controller;
+
+import subway.domain.LineRepository;
+import subway.domain.StationRepository;
+import subway.utils.Message;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+import java.util.Scanner;
+
+public class StationController {
+    private static String command;
+    private static StationRepository stationRepository;
+    private static LineRepository lineRepository;
+
+    public StationController(String command, StationRepository stationRepository, LineRepository lineRepository) {
+        this.command = command;
+        this.stationRepository = stationRepository;
+        this.lineRepository = lineRepository;
+    }
+
+    public static void play(Scanner scanner) {
+        if (command.equals("1"))
+            registerStation(scanner);
+        else if (command.equals("2"))
+            deleteStation(scanner);
+        else if (command.equals("3"))
+            getStationList();
+    }
+
+    public static void registerStation(Scanner scanner) {
+        InputView.getStation(scanner, stationRepository);
+    }
+
+    public static void getStationList() {
+        OutputView.printStationsInfo(stationRepository);
+    }
+
+    public static void deleteStation(Scanner scanner) {
+        String input = InputView.getDelStation(scanner, stationRepository, lineRepository);
+        stationRepository.deleteStation(input);
+    }
+
+    public static StationRepository getNewStationRepository() {
+        return stationRepository;
+    }
+}
