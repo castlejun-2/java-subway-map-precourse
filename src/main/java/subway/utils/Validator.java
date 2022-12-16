4c29420bdb51f2493d2f7468adea5d26fa4b0fbf
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 00:35:38 +0900

    feat: 삭제하는 역의 입력값의 예외처리

--- src/main/java/subway/utils/Validator.java
+++ src/main/java/subway/utils/Validator.java
@@ -2,6 +2,8 @@ package subway.utils;
 
 import subway.domain.Line;
 import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 import subway.utils.MainMenu;
 import subway.utils.Message;
 
@@ -18,5 +20,45 @@ public class Validator {
         }
         if (!flag)
             throw new IllegalArgumentException(Message.COMMAND_ERROR);
+    }
+
+    public static boolean checkDetailCommand(String input, String inputData) {
+        if (inputData.equals("B"))  //알파벳 B이면 정상 반환
+            return true;
+        for (Character c : inputData.toCharArray()) {   //B가 아닌 알파벳이 들어가 있는 경우 에러 반환
+            if (Character.isAlphabetic(c))
+                throw new IllegalArgumentException(Message.COMMAND_ERROR);
+        }
+        //숫자가 주어진 명령어의 갯수보다 많은 경우 에러 반환
+        int cmdLength = ServiceMenuDetail.findMessageByCommand(input).length;
+
+        if (cmdLength <= Integer.parseInt(inputData) || cmdLength == 0)
+            throw new IllegalArgumentException(Message.COMMAND_ERROR);
+        return true;
+    }
+
+    public static void checkStationExist(String input, StationRepository stationRepository) {
+        for (Station station : stationRepository.stations()) {
+            if (station.getName().equals(input))
+                throw new IllegalArgumentException(Message.STATION_IS_EXIST);
+        }
+    }
+
+    public static void checkStationExistLine(String input,StationRepository stationRepository, LineRepository lineRepository) {
+        for (Line line : lineRepository.lines()) {
+            for (Station station : line.stations()) {
+                if (station.getName().equals(input))
+                    throw new IllegalArgumentException(Message.EXIST_STATION_IS_NOT_DELETE);
+            }
+        }
+        boolean flag = true;
+        for (Station station : stationRepository.stations()) {
+            if (station.getName().equals(input)) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+            throw new IllegalArgumentException(Message.STATION_IS_NOT_EXIST);
     }
 }
