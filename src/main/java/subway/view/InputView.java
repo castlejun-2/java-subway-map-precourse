1620fab26ee615663c71be88651af8c651c2da9b
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 00:35:57 +0900

    feat: 역을 관리하는 삭제 기능 구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -2,6 +2,8 @@ package subway.view;
 
 import subway.domain.Line;
 import subway.domain.LineRepository;
+import subway.domain.Station;
+import subway.domain.StationRepository;
 import subway.utils.Message;
 import subway.utils.Validator;
 import subway.view.OutputView;
@@ -19,6 +21,46 @@ public class InputView {
         } catch (IllegalArgumentException e) {
             OutputView.printError(e.getMessage());
             return getMainCmd(scanner);
+        }
+    }
+
+    public static String getDetailCmd(String input, Scanner scanner) {
+        OutputView.printServiceMenu(input);
+        OutputView.println(Message.SELECT_FUNCTION_MESSAGE);
+        String inputData = scanner.nextLine();
+        try {
+            Validator.checkDetailCommand(input, inputData);
+            return inputData;
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return getDetailCmd(input, scanner);
+        }
+    }
+
+    public static boolean getStation(Scanner scanner, StationRepository stationRepository) {
+        OutputView.println(Message.STATION_INPUT_MESSAGE);
+        String inputData = scanner.nextLine();
+        try {
+            Validator.checkStationExist(inputData, stationRepository);
+            stationRepository.addStation(new Station(inputData));
+            OutputView.println(Message.STATION_ADD_SUCCESS_MESSAGE);
+            return true;
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return getStation(scanner, stationRepository);
+        }
+    }
+
+    public static String getDelStation(Scanner scanner, StationRepository stationRepository, LineRepository lineRepository) {
+        OutputView.println(Message.DELETE_STATION_INPUT_MESSAGE);
+        String inputData = scanner.nextLine();
+        try {
+            Validator.checkStationExistLine(inputData, stationRepository, lineRepository);
+            OutputView.println(Message.STATION_DELETE_SUCCESS_MESSAGE);
+            return inputData;
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return getDelStation(scanner, stationRepository, lineRepository);
         }
     }
 }
