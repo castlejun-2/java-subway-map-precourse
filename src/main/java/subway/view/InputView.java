5d4f337c66aba550b1b5c74d742f7d7161b1675d
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 01:21:16 +0900

    feat: 지하철 노선 등록 입력 기능 구현

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -37,14 +37,13 @@ public class InputView {
         }
     }
 
-    public static boolean getStation(Scanner scanner, StationRepository stationRepository) {
+    public static String getStation(Scanner scanner, StationRepository stationRepository) {
         OutputView.println(Message.STATION_INPUT_MESSAGE);
         String inputData = scanner.nextLine();
         try {
             Validator.checkStationExist(inputData, stationRepository);
-            stationRepository.addStation(new Station(inputData));
             OutputView.println(Message.STATION_ADD_SUCCESS_MESSAGE);
-            return true;
+            return inputData;
         } catch (IllegalArgumentException e) {
             OutputView.printError(e.getMessage());
             return getStation(scanner, stationRepository);
@@ -61,6 +60,43 @@ public class InputView {
         } catch (IllegalArgumentException e) {
             OutputView.printError(e.getMessage());
             return getDelStation(scanner, stationRepository, lineRepository);
+        }
+    }
+
+    public static String getDelLine(Scanner scanner, LineRepository lineRepository) {
+        OutputView.println(Message.DELETE_LINE_INPUT_MESSAGE);
+        String inputData = scanner.nextLine();
+        try {
+            Validator.checkExistLine(inputData, lineRepository);
+            OutputView.println(Message.LINE_DELETE_SUCCESS_MESSAGE);
+            return inputData;
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return getDelLine(scanner, lineRepository);
+        }
+    }
+
+    public static Line getLine(Scanner scanner, LineRepository lineRepository) {
+        OutputView.println(Message.ADD_LINE_INPUT_MESSAGE);
+        try {
+            String inputLineName = scanner.nextLine();
+            Validator.checkLineExist(inputLineName, lineRepository);
+            Line line = new Line(inputLineName);
+
+            OutputView.println(Message.ADD_LINE_TOP_STATION_INPUT_MESSAGE);
+            String inputTopStation = scanner.nextLine();
+            line.addStationByIndex(new Station(inputTopStation),0);
+
+            OutputView.println(Message.ADD_LINE_BOTTOM_STATION_INPUT_MESSAGE);
+            String inputBottomStation = scanner.nextLine();
+            line.addStationByIndex(new Station(inputBottomStation),0);
+
+            OutputView.println(Message.LINE_ADD_SUCCESS_MESSAGE);
+            System.out.println("");
+            return line;
+        } catch (IllegalArgumentException e) {
+            OutputView.printError(e.getMessage());
+            return getLine(scanner, lineRepository);
         }
     }
 }
