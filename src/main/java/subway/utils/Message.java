b180cd6aa8c0f48f7d5cefbe93b30ad32ae354ff
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-12-17 00:36:51 +0900

    feat: 삭제하는 역의 입력 문자열 상수

--- src/main/java/subway/utils/Message.java
+++ src/main/java/subway/utils/Message.java
@@ -5,6 +5,17 @@ public class Message {
 
     public static final String SELECT_FUNCTION_MESSAGE = "원하는 기능을 선택하세요.";
     public static final String LINE_INPUT_MESSAGE = "노선을 입력하세요.";
+    public static final String STATION_INPUT_MESSAGE = "등록할 역 이름을 입력하세요.";
+    public static final String STATION_ADD_SUCCESS_MESSAGE = "지하철 역이 등록되었습니다.";
+    public static final String STATION_DELETE_SUCCESS_MESSAGE = "지하철 역이 삭제되었습니다.";
+    public static final String EXIST_STATION_IS_NOT_DELETE = "노선에 등록된 역은 삭제할 수 없습니다.";
+    public static final String STATION_IS_NOT_EXIST = "존재하지 않는 지하철 역입니다.";
+    public static final String DELETE_STATION_INPUT_MESSAGE = "삭제할 역 이름을 입력하세요.";
+
+    public static final String[] STATION_DETAIL_MESSAGE = {"1. 역 등록" , "2. 역 삭제", "3. 역 조회", "B. 돌아가기"};
+    public static final String[] LINE_DETAIL_MESSAGE = {"1. 노선 등록", "2. 노선 삭제", "3. 노선 조회", "B. 돌아가기"};
+    public static final String[] SECTION_DETAIL_MESSAGE = {"1. 구간 등록", "2. 구간 삭제", "B. 돌아가기"};
 
     public static final String LINE_IS_NOT_EXIST = "존재하지 않는 노선입니다.";
+    public static final String STATION_IS_EXIST = "이미 존재하는 역입니다";
 }
